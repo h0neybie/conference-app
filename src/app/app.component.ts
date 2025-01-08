ccf0a62fb2a455a121600a77dcdeb08729170e5c
h0neybie <dizonabbycruz@gmail.com>
2025-01-08 15:03:21 +0800

    this is a push- pre exam

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PartnersComponent } from './partners/partners.component';
+import { JoinComponent } from './join/join.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomeComponent,AboutComponent,PartnersComponent, JoinComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
